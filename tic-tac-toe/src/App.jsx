6e74e28c76c42a644fff89ba9da1431c0c04aae0
evita <evita.dsilva03@gmail.com>
2025-06-02 17:18:10 +0530

    Build basic ui

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 
+const initialBoard = () => Array(9).fill(null);
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [board, setboard] = useState(initialBoard());
 
   return (
-      <div>Hello world</div>
+      <div className='game'>
+        <div className='status'>
+          Player X turn
+        <button className='reset-button'>Reset Game</button>
+        </div>
+
+      <div className='board'>
+        {board.map((_,index) => {
+          return <button className='cell' key={index}>X</button>
+        })}
+      </div>
+
+      </div>
   )
 }
 
